0ffa675bd094d1b4d98c6c360dd72f1f5303a3ed
"pablo" <"paulbridge86uk@gmail.com">
2022-02-04 22:14:28 +0000

    2nd

---
+++ contracts/3_fund_me.sol
@@ -0,0 +1,24 @@
+ // SPDX-License-Identifier: MIT
+ pragma solidity ^ 0.6.0;
+ import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+
+ contract FundMe {
+
+      mapping(address => uint256) public addressToAmountFunded;
+      function fund() public payable  {
+      addressToAmountFunded[msg.sender] += msg.value;
+      // ETH TO USD CONVERSION RATE
+         
+      }
+
+      function getVersion() public view  returns (uint256) {
+          AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+          return priceFeed.version();
+      }
+
+      function getPrice() public view returns(uint256) {
+          AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+          (,int256 answer,,,) = priceFeed.latestRoundData();
+          return uint256(answer);
+      }
+ } 
