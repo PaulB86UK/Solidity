c3dc6d4f08f8dfe7c4b3abf22f8574d854f451d8
"pablo" <"paulbridge86uk@gmail.com">
2022-02-05 10:57:43 +0000

    2nd

--- contracts/3_fund_me.sol
+++ contracts/3_fund_me.sol
@@ -19,6 +19,12 @@
       function getPrice() public view returns(uint256) {
           AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
           (,int256 answer,,,) = priceFeed.latestRoundData();
-          return uint256(answer);
+          return uint256(answer * 10000000000);
       }
+
+       function getConversionRate(uint256 ethAmount) public view returns(uint256) {
+           uint256 ethPrice = getPrice();
+           uint256 ethAMountinUsd = (ethPrice * ethAmount);
+           return ethAMountinUsd;
+       }
  } 
